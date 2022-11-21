5258acb2cb7932650ffe2f788c17ea0a0af193ee
Thomalita Burbank <thomalita37@gmail.com>
2022-11-21 10:29:58 -0700

    created routes

---
+++ config/routes/ClassRouter.js
@@ -0,0 +1,12 @@
+const Router = require('express').Router()
+const controller = require('../controllersclassController')
+
+Router.get('/', controller.GetAllClass)
+Router.post('/', controller.CreateClass)
+Router.put('/class_id', controller.UpdateClass)
+Router.delete('/class_id', controller.DeleteClass)
+Router.get('/class_id', controller.getClassById)
+
+
+
+module.exports = Router
